d4067cb044505ef59fecfbf9fa008b55dbb234a8
Lorenzo <cordalorenzowork@gmail.com>
2025-10-15 20:47:24 +0200

    adding text about pedagogistpage

--- pages/PedagogistPage/PedagogistPage.jsx
+++ pages/PedagogistPage/PedagogistPage.jsx
@@ -10,36 +10,37 @@ const PedagogistPage = () => {
     <>
       <Navigation />
       <div>
-        <div className="Container">
-          <div className="Row">
+        <div className="container">
+          <div className="row">
             <h2 className="d-flex justify-content-center mt-3 mb-3 ">
               La Pedagogia Familiare®️
             </h2>
+            <div className="col-12 col-md-6">
+              <img className="custom-square" src="/Photo/Square.png" alt="" />
+            </div>
+            <div className="col-12 col-md-6">
+              <p className="custom-p">
+                La Pedagogia Familiare®️ è stata fondata 40 anni fa dalla
+                Prof.ssa Vincenza Palmieri e si è evoluta negli anni ottenendo
+                grandi e importanti riconoscimenti.
+              </p>
+            </div>
+          </div>
+          <div className="row">
             <div className="col-md-6 col-12">
-              <div className="d-flex">
-                <img className="custom-square" src="/Photo/Square.png" alt="" />
-
-                <p className="d-flex justify-content-center  align-content-center custom-p">
-                  La Pedagogia Familiare®️ è stata fondata 40 anni fa dalla
-                  Prof.ssa Vincenza Palmieri e si è evoluta negli anni ottenendo
-                  grandi e importanti riconoscimenti.
-                </p>
-              </div>
+              <p className="custom-p">
+                Tale riconoscimento è stato esteso anche all'Associazione
+                Nazionale dei Pedagogisti Familiari, fondata nel 2010 come
+                Associazione di riferimento per i Pedagogisti Familiari e che ne
+                tutela la formazione e la professionalità. La Pedagogia
+                Familiare®️ è la scienza che rappresenta l'intera rete di
+                strumenti e processi volti a migliorare la qualità del primo
+                micro-cosmo di riferimento - la Famiglia - e dei suoi
+                componenti, intesi sia come singoli sia come gruppo.
+              </p>
             </div>
             <div className="col-md-6 col-12">
-              <div className="fs-5">
-                Nel 2019, infatti, il Ministero per lo Sviluppo Economico (MiSE)
-                riconosce la Professione come autonoma e originale, definendola
-                come una Scienza e una Disciplina a sé. Tale riconoscimento è
-                stato esteso anche all'Associazione Nazionale dei Pedagogisti
-                Familiari, fondata nel 2010 come Associazione di riferimento per
-                i Pedagogisti Familiari e che ne tutela la formazione e la
-                professionalità. La Pedagogia Familiare®️ è la scienza che
-                rappresenta l'intera rete di strumenti e processi volti a
-                migliorare la qualità del primo micro-cosmo di riferimento - la
-                Famiglia - e dei suoi componenti, intesi sia come singoli sia
-                come gruppo.
-              </div>
+              <img className="custom-bridge" src="/Photo/Bridge.png" alt="" />
             </div>
           </div>
           <div className="custom-btn mb-4">
