1537a0c1257d017bf67f49162121441343f8c456
Lorenzo <cordalorenzowork@gmail.com>
2025-10-20 17:50:20 +0200

    add pedagogist section

--- pages/PedagogistPage/PedagogistPage.jsx
+++ pages/PedagogistPage/PedagogistPage.jsx
@@ -12,35 +12,32 @@ const PedagogistPage = () => {
       <div>
         <div className="container">
           <div className="row">
-            <h2 className="d-flex justify-content-center mt-3 mb-3 ">
+            <h2 className="d-flex justify-content-center mt-4 mb-4 ">
               La Pedagogia Familiare®️
             </h2>
-            <div className="col-12 col-md-6">
+            <div className="col-6 col-md-6">
               <img className="custom-square" src="/Photo/Square.png" alt="" />
             </div>
-            <div className="col-12 col-md-6">
-              <p className="custom-p">
-                La Pedagogia Familiare®️ è stata fondata 40 anni fa dalla
-                Prof.ssa Vincenza Palmieri e si è evoluta negli anni ottenendo
-                grandi e importanti riconoscimenti.
+            <div className="col-6 col-md-6">
+              <p className="custom-p text-center">
+                La Pedagogia Familiare®️, fondata oltre 40 anni fa dalla
+                Prof.ssa Vincenza Palmieri, è una scienza che mira a migliorare
+                la qualità della vita familiare e dei suoi componenti.
               </p>
             </div>
           </div>
           <div className="row">
-            <div className="col-md-6 col-12">
-              <p className="custom-p">
-                Tale riconoscimento è stato esteso anche all'Associazione
-                Nazionale dei Pedagogisti Familiari, fondata nel 2010 come
-                Associazione di riferimento per i Pedagogisti Familiari e che ne
-                tutela la formazione e la professionalità. La Pedagogia
-                Familiare®️ è la scienza che rappresenta l'intera rete di
-                strumenti e processi volti a migliorare la qualità del primo
-                micro-cosmo di riferimento - la Famiglia - e dei suoi
-                componenti, intesi sia come singoli sia come gruppo.
+            <div className="col-6 col-md-6">
+              <p className="custom-p text-center">
+                Nel 2010 è nata l’Associazione Nazionale dei Pedagogisti
+                Familiari, che ne tutela la formazione e la professionalità,
+                riconoscendo il valore e l’evoluzione di questa disciplina.
               </p>
             </div>
-            <div className="col-md-6 col-12">
-              <img className="custom-bridge" src="/Photo/Bridge.png" alt="" />
+            <div className="col-6 col-md-6">
+              <div className="d-flex align-items-center justify-content-center h-100">
+                <img className="custom-bridge" src="/Photo/Bridge.png" alt="" />
+              </div>
             </div>
           </div>
           <div className="custom-btn mb-4">
--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,21 @@
 import HomePage from "../pages/HomePage";
+import { useEffect } from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-
 import "./App.css";
 import PedagogistPage from "../pages/PedagogistPage/PedagogistPage";
 import WhoIsPedagogistPage from "../pages/WhoIsThePedagogist/WhoIsPedagogistPage";
 import EffectiveTeaching from "../pages/EffectiveTeaching/EffectiveTeaching";
 import AboutMePage from "../pages/AboutMe/AboutMePage";
+import AOS from "aos";
+import "aos/dist/aos.css";
 
 const App = () => {
+  useEffect(() => {
+    AOS.init({
+      duration: 1000, // durata in ms
+      once: true, // se true, l’animazione avviene solo la prima volta
+    });
+  }, []);
   return (
     <>
       <BrowserRouter>
